a984d9d476ba0ee60c9208a14b4c967a85a53e0b
unknown <1064592074@qq.com>
2022-04-29 17:33:00 +0800

    redis

--- mybatis/src/main/java/com/tdy/inf/plugin/ExamplePlugin.java
+++ mybatis/src/main/java/com/tdy/inf/plugin/ExamplePlugin.java
@@ -1,2 +1,23 @@
-package com.tdy.inf.plugin;public class ExamplePlugin {
+package com.tdy.inf.plugin;
+
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+
+@Intercepts(@Signature(type = Executor.class,method = "query",args = {MappedStatement.class,Object.class, RowBounds.class, ResultHandler.class}))
+public class ExamplePlugin implements Interceptor {
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+
+        System.out.println("before:"+invocation);
+        Object returnObject = invocation.proceed();
+        System.out.println("after:"+returnObject);
+
+        return returnObject;
+    }
 }
--- redis/src/main/java/com/tdy/RedisTest.java
+++ redis/src/main/java/com/tdy/RedisTest.java
@@ -1,2 +1,53 @@
-package com.tdy;public class RedisTest {
+package com.tdy;
+
+import org.springframework.data.redis.connection.RedisNode;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * 1、JedisPoolConfig
+ * 2、redisSentinelConfiguration
+ *        设置哨兵节点、主节点名称
+ * 3、JedisConnectionFactory
+ * 4、StringRedisTemplate
+ */
+public class RedisTest {
+
+    public static void main(String[] args) {
+
+
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+
+        RedisNode redisNode1 = new RedisNode("192.168.161.95", 26379);
+        RedisNode redisNode2 = new RedisNode("192.168.161.95", 26380);
+        RedisNode redisNode3 = new RedisNode("192.168.161.95", 26381);
+
+//        RedisNode redisNode = new RedisNode();
+
+        RedisSentinelConfiguration redisSentinelConfiguration = new RedisSentinelConfiguration();
+        redisSentinelConfiguration.setMaster("mymaster");
+
+        redisSentinelConfiguration.addSentinel(redisNode1);
+        redisSentinelConfiguration.addSentinel(redisNode2);
+        redisSentinelConfiguration.addSentinel(redisNode3);
+
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(redisSentinelConfiguration,poolConfig);
+        jedisConnectionFactory.setPassword("Tydk@202109");
+        jedisConnectionFactory.afterPropertiesSet();
+
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(jedisConnectionFactory);
+        stringRedisTemplate.afterPropertiesSet();
+
+
+//        stringRedisTemplate.opsForValue().set("aaaa","bbaaa");
+        String aaaa = stringRedisTemplate.opsForValue().get("aaaa");
+
+        System.out.println(aaaa);
+
+
+    }
+
 }
